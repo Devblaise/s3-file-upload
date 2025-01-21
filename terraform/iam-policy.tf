ee3b3c2d678bc9d16ac81a3066b7829714ba0bad
Devblaise <blaisembadugha@gmail.com>
2025-01-21 19:50:35 +0100

    update pipline

--- terraform/iam-policy.tf
+++ terraform/iam-policy.tf
@@ -1,7 +1,7 @@
 resource "aws_iam_role_policy_attachment" "iam_policy" {
   role = aws_iam_role.lambda_exec_role.name
   # Just for testing purpose, don't try to give administrator access in production
-  # policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  # policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 
 }
