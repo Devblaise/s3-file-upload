ee94516d80b71b0d2c5b9927e7ddcff07ebcf3e0
Devblaise <blaisembadugha@gmail.com>
2025-01-21 20:07:07 +0100

    update pipeline config

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,14 +3,13 @@ resource "aws_s3_bucket" "bucket_in" {
   bucket = "${var.s3_bucket_prefix}-in"
 }
 
-
 resource "aws_s3_bucket_cors_configuration" "bucket_in_cors" {
   bucket = aws_s3_bucket.bucket_in.id
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST"]
-    allowed_origins = ["*"]
+    allowed_methods = ["GET", "PUT", "POST", "OPTIONS"]
+    allowed_origins = ["http://localhost:3000"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
@@ -21,14 +20,13 @@ resource "aws_s3_bucket" "bucket_out" {
   bucket = "${var.s3_bucket_prefix}-out"
 }
 
-
 resource "aws_s3_bucket_cors_configuration" "bucket_out_cors" {
   bucket = aws_s3_bucket.bucket_out.id
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST"]
-    allowed_origins = ["*"]
+    allowed_methods = ["GET", "PUT", "POST", "OPTIONS"]
+    allowed_origins = ["http://localhost:3000"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
