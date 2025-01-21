9caf010df8503b5da7a9a92ea96d0289be03a381
Devblaise <blaisembadugha@gmail.com>
2025-01-21 21:46:11 +0100

    update pipeline config

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -13,6 +13,11 @@ resource "aws_s3_bucket_cors_configuration" "bucket_in_cors" {
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
 }
 
 # S3 bucket for processed files ("bucket-out")
@@ -29,5 +34,9 @@ resource "aws_s3_bucket_cors_configuration" "bucket_out_cors" {
     allowed_origins = ["http://localhost:3000"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
+  }
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
   }
 }
