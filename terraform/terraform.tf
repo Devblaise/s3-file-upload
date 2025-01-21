c5633b2fa991124f6a2d00853890c2f0be6f7fa6
Devblaise <blaisembadugha@gmail.com>
2025-01-21 19:33:07 +0100

    fix terraform format

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,22 +1,22 @@
-terraform { 
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = ">=5.25"
-      }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.25"
     }
-    backend "remote" { 
-    
-    organization = "myDevOpsProjects" 
+  }
+  backend "remote" {
 
-    workspaces { 
-      name = "s3-upload" 
-    } 
-  } 
-   required_version = ">=1.10.4"
+    organization = "myDevOpsProjects"
+
+    workspaces {
+      name = "s3-upload"
+    }
+  }
+  required_version = ">=1.10.4"
 }
 provider "aws" {
-  region = var.region
+  region     = var.region
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
